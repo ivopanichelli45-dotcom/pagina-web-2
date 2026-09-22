13668e7710b0a6367f139a84aa35f3e50e199e1d
Ivo Panichelli <ivopanichelli45@gmail.com>
2026-09-22 17:16:28 -0300

    se probo el Home que funcione el header y navbar

--- mi-proyecto/src/pages/Home.js
+++ mi-proyecto/src/pages/Home.js
@@ -1,1 +1,11 @@
-/*PAGINA PRINCIPAL*/
+import "../components/header.js";
+import "../components/Navbar.js";
+
+console.log("home.js funciona");
+
+const app = document.querySelector("#app");
+
+app.innerHTML = `
+<ultratech-header></ultratech-header>
+<ultratech-navbar></ultratech-navbar>
+`;
